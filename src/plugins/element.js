0b9430ed00e61866b5f02f1b67c13de7fb285440
WindZhai <zhaiyangyang19@dnui.edu.cn>
2020-12-15 19:30:38 +0800

    完成登录

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,4 +1,9 @@
 import Vue from 'vue'
-import { Button } from 'element-ui'
+import { Button, Form, FormItem, Input, Message } from 'element-ui'
 
 Vue.use(Button)
+Vue.use(Form)
+Vue.use(FormItem)
+Vue.use(Input)
+// message功能需要挂载到vue中
+Vue.prototype.$message = Message
